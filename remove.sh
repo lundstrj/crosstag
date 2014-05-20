61173db429f1218f28a6f5ed0e7de29dab3d7283
Johan Lundstroem <lundstrom.se@gmail.com>
2014-05-20 22:55:09 +0200

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -6,5 +6,4 @@ sudo pip uninstall pyserial
 sudo pip uninstall flask-security 
 sudo pip uninstall flask-sqlalchemy
 sudo pip uninstall Flask-WTF
-rm crosstag.db
-#sudo deluser crosstag
+rm -r /home/pi/crosstag
