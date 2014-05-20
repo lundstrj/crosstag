88b49e7f663658fd4ed870d29a4017c3aff36c47
Johan Lundstroem <lundstrom.se@gmail.com>
2014-05-20 10:33:55 +0200

    Update remove.sh
    Made it useful

--- remove.sh
+++ remove.sh
@@ -1,1 +1,12 @@
-pip uninstall
+sudo killall python
+cd /home/crosstag
+
+sudo pip uninstall requests
+sudo pip uninstall pyserial
+sudo pip uninstall flask-security 
+sudo pip uninstall flask-sqlalchemy
+sudo pip uninstall Flask-WTF
+rm crosstag.db
+cd
+sudo deluser crosstag
+sudo rm -r /home/crosstag
