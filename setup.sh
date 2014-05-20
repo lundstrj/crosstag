cf88361a767e7f4a52d4fff61b1ff4cf1f066f63
Johan Lundstroem <lundstrom.se@gmail.com>
2014-05-20 10:19:03 +0200

    Update setup.sh
    Added the actual dependencies and maybe even made this a runnable script. Who knows?

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
+sudo adduser crosstag
+cd /home/crosstag
+git clone https://github.com/lundstrj/crosstag.git
+cd crosstag
+mv * ../
+cd ..
+
 sudo apt-get install python-pip
-sudo pip install flask-sqlalchemy
 sudo pip install requests
-# sudo pip install pysqlite
 sudo pip install pyserial
+sudo pip install flask-security 
+sudo pip install flask-sqlalchemy
+sudo pip install Flask-WTF
+touch crosstag.db
