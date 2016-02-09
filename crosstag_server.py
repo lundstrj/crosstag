3bcb9d5d6eb5e417ddc6231902844668a20c1616
Filip Rydberg <fr222cy@student.lnu.se>
2016-02-09 14:27:01 +0100

    Removed some lines frome previous commit, to get the application running.

--- crosstag_server.py
+++ crosstag_server.py
@@ -80,10 +80,10 @@ class User(db.Model):
     birth_date = db.Column(db.Date)
     expiry_date = db.Column(db.Date)
     create_date = db.Column(db.Date)
-    status = db.Column(db.Integer, db.ForeignKey('member_status.id'))
+    #status = db.Column(db.Integer, db.ForeignKey('member_status.id'))
 
     def __init__(self, name, email, phone=None, tag_id=None, fortnox_id=None,
-                 expiry_date=None, birth_date=None, gender=None, status=None):
+                 expiry_date=None, birth_date=None, gender=None):
         self.name = name
         self.email = email
         self.phone = phone
@@ -93,7 +93,7 @@ class User(db.Model):
         self.birth_date = birth_date
         self.gender = gender
         self.create_date = datetime.now()
-        self.status = status
+        #self.status = status
 
     def dict(self):
         return {'index': self.index, 'name': self.name,
@@ -103,7 +103,7 @@ class User(db.Model):
                 'create_date': str(self.create_date),
                 'birth_date': str(self.birth_date),
                 'gender': self.gender,
-                'status': str(self.status)}
+                }
 
 
     def json(self):
