ec9bdb52e245634af198140830b2002c777c7def
Patrik <pe22dw@student.lnu.se>
2016-02-11 14:14:31 +0100

    added some code in read_from_fortnox.py

--- read_from_fortnox.py
+++ read_from_fortnox.py
@@ -1,7 +1,20 @@
+import requests
 
 class ReadFromFortnox:
 
-    def get_all_users(self):
-        users = []
+    def send_request(self):
 
-        return users
+            try:
+                r = requests.get(
+                    url="https://api.fortnox.se/3/customers",
+                    headers = {
+                        "Access-Token":"",
+                        "Client-Secret":"",
+                        "Content-Type":"application/json",
+                        "Accept":"application/json",
+                    },
+                )
+                print('Response HTTP Status Code : {status_code})'.format(status_code=r.status_code))
+                print('Response HTTP Response Body : {content})'.format(content=r.content))
+            except requests.exceptions.RequestException as e:
+                print('HTTP Request failed')
