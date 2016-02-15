e4d85a847ec9b7fd6c0bafda95b1b9e7e49ace23
kn222gp <kn222gp@student.lnu.se>
2016-02-15 16:38:23 +0100

    feat: Can insert users into fortnox database now. Updated the get function, using requests now.

--- fortnox.py
+++ fortnox.py
@@ -1,5 +1,6 @@
 import http.client
 import json
+import requests
 import fortnox_cfg as cfg
 from pyfiglet import Figlet
 import os
@@ -9,19 +10,14 @@ class Fortnox:
 
     def get_all_customers(self):
 
-        connection = http.client.HTTPSConnection('api.fortnox.se')
-        connection.request('GET', '/3/customers/', None, cfg.fortnox)
-
         try:
-            response = connection.getresponse()
-            content = response.read()
-            # Success
-            print('Response status ' + str(response.status))
-            print (content)
-
-            str_response = content.decode('utf-8')
-            obj = json.loads(str_response)
-            return obj["Customers"]
+            r = requests.get(
+                url = 'https://api.fortnox.se/3/customers',
+                headers = cfg.fortnox
+            )
+            print('Response status: {status_code}'.format(status_code=r.status_code))
+            content = json.loads(r.text)
+            return content["Customers"]
         except http.client.HTTPException:
             # Exception
             print('Exception during request')
@@ -43,7 +39,31 @@ class Fortnox:
             print('Exception during request')
 
 
-
+    def insert_customer(self, user):
+        try:
+            r = requests.post(
+                url = 'https://api.fortnox.se/3/customers',
+                headers = cfg.fortnox,
+                data = json.dumps({
+                    "Customer": {
+                        "Name": user.name,
+                        "Email": user.email,
+                        "Address1": user.address,
+                        "Address2": user.address2,
+                        "City": user.city,
+                        "ZipCode": user.zip_code
+                    }
+                })
+            )
+            print('Response status: {status_code}'.format(status_code=r.status_code))
+            print(user.name)
+            print(user.email)
+            print(user.address)
+            print(user.address2)
+            print(user.city)
+            print(user.zip_code)
+        except http.client.HTTPException as e:
+            print('Exception during POST-request')
 
 
 
