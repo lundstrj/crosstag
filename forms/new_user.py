c16ca235a582e9c7dc8aebaf2591de9af7a9f760
ej222pj <ej222pj@student.lnu.se>
2016-02-18 11:23:49 +0100

    feat: adding validation to edit user

--- forms/new_user.py
+++ forms/new_user.py
@@ -1,25 +1,18 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, RadioField, DateField
+from wtforms import TextField, RadioField, DateField, validators
 from wtforms.validators import Required
 
 
 class NewUser(Form):
-    name = TextField('name', validators=[])
-    email = TextField('email', validators=[])
+    name = TextField('name', [validators.Length(max=80), validators.DataRequired()])
+    email = TextField('email', [validators.Length(max=120), validators.Email()])
     phone = TextField('phone', validators=[])
-    address = TextField('address', validators=[])
-    address2 = TextField('address2', validators=[])
-    city = TextField('city', validators=[])
+    address = TextField('address', [validators.Length(max=50), validators.DataRequired()])
+    address2 = TextField('address2', [validators.Length(max=50)])
+    city = TextField('city', [validators.Length(max=120), validators.DataRequired()])
     zip_code = TextField('zip_code', validators=[])
-    fortnox_id = TextField('fortnox_id', validators=[])
     tag_id = TextField('tag_id', validators=[])
-    expiry_date = DateField('expiry_date', validators=[], format='%Y-%m-%d',
-                            description="DESC1")
+    expiry_date = DateField('expiry_date', validators=[], format='%Y-%m-%d', description="DESC1")
     birth_date = DateField('birth_date', format='%Y-%m-%d', validators=[])
-    gender = RadioField(
-        'gender',
-        [Required()],
-        choices=[('male', 'male'), ('female', 'female'),
-                 ('unknown', 'unknown')], default='unknown'
-
-    )
+    gender = RadioField('gender', [validators.DataRequired()], choices=[('male', 'male'), ('female', 'female'),
+                                                                        ('unknown', 'unknown')])
