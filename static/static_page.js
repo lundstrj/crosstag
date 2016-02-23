d929dadbe6a8ffdb35f24a9945fc3952f8aee875
Kristoffer Svensson <kristoffer@KS.crosstag>
2016-02-23 15:57:23 +0100

    Adding some begnning of the feature of a static page when tagging in. This commit is made by eric and stoffe

---
+++ static/static_page.js
@@ -0,0 +1,111 @@
+window.onload = function() {
+
+    var last_event = null;
+    var counter = 0;
+    var display_user = false;
+    var user_data = null;
+    var user_tagins = null;
+    var display_time = 20;
+    var sleep_time = 2;
+    var current_user = null;
+
+
+    function poll_server(callback) {
+        try {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://localhost:80/crosstag/v1.0/last_tagin", true);
+            xhr.addEventListener("load", function(){
+                callback(CheckTagins, JSON.parse(xhr.response));
+            });
+            xhr.send(null);
+        }
+        catch (exception){
+            return null
+        }
+    }
+
+    function print_user(user_data, user_tagins){
+        if(is_not_empty(user_data)){
+            document.getElementById("tagins").innerHTML = user_data.name;
+        }
+
+    }
+
+    function get_user_data(callback, tag_nbr) {
+        try {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "http://localhost:80/crosstag/v1.0/get_user_data_tag/" + tag_nbr, true);
+            xhr.addEventListener("load", function(){
+                callback(JSON.parse(xhr.response));
+            });
+            xhr.send(null);
+        }
+        catch(exception) {
+            return null
+        }
+    }
+
+    function set_user_data(data){
+        user_data = data;
+    }
+
+    function check_if_tagevent_exists(callback, current){
+        if (current == null) {
+            display_user = false;
+            console.log("current = null")
+        }
+        else if(is_not_empty(current)){
+            if (current['index'] != last_event) {
+                last_event = current['index'];
+                display_user = true;
+                counter = 0;
+                current_user = current;
+            }
+            callback()
+        }
+    }
+
+    function is_not_empty(object){
+        for(var key in object){
+            if(object.hasOwnProperty(key)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function CheckTagins() {
+        if (display_user && counter >= 0) {
+            get_user_data(set_user_data, current_user['tag_id'])
+        }
+
+        if (!user_data && display_user) {
+            //print("read tag: %s" % current['tag_id'])
+            display_user = false
+        }
+
+        if (display_user && user_data && counter == 0) {
+            //this.print_user(this.user_data, this.user_tagins);
+            print_user(user_data, user_tagins);
+            counter += 1
+        }
+
+        if (display_user && user_data && counter != 0) {
+            counter += 1;
+            //#print this.display_time - this.counter * this.sleep_time
+        }
+
+        if (counter >= display_time / sleep_time) {
+            counter = 0;
+            display_user = false;
+            user_data = null;
+            document.getElementById("tagins").innerHTML = "ingen tag";
+            //this.print_clear_screen("online")
+        }
+    }
+
+
+    setInterval(function(){
+        poll_server(check_if_tagevent_exists);
+    }, 1000)
+}
